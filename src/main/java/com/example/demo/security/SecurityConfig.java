c7ba1905a54b28c730fd1f57bcfb71ac5c3d73fb
DaliaMahmoud-sudo <dalia.m3544214@gmail.com>
2026-03-10 17:22:05 +0200

    add frontend url

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -59,7 +59,7 @@ private final JwtFilter jwtFilter;
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://secure-funds-transfer-frontend.vercel.app"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
